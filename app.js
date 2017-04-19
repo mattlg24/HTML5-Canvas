ce94105bcf47e543ebc84f5f2e4750f909321ebf
Matt Gardner <matthewlgardner@gmail.com>
2017-04-19 13:30:42 -0600

    line starts from last offsetX and last offsetY

--- app.js
+++ app.js
@@ -8,24 +8,36 @@ canvas.height = window.innerHeight
 ctx.strokeStyle = '#BADA55'
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
+ctx.lineWidth = 10
 
 let isDrawing = false
 let lastX = 0
 let lastY = 0
+let hue = 0
 
 function draw(e) {
+  ctx.strokeStyle = 'hsl(240, %100, %50)'
   if (!isDrawing) {
     return //this stops the function from running when they are not moused down
   }
   console.log(e);
   ctx.beginPath()
+  //start from
   ctx.moveTo(lastX, lastY)
+  //go to
   ctx.lineTo(e.offsetX, e.offsetY)
   ctx.stroke()
+  lastX = e.offsetX
+  lastY = e.offsetY
 }
 
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true
+  lastX = e.offsetX
+  lastY = e.offsetY
+})
+
 canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mousedown', () => isDrawing = true)
 canvas.addEventListener('mouseup', () => isDrawing = false)
 canvas.addEventListener('mouseout', () => isDrawing = false)
 
